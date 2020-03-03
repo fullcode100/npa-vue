a925aeb6bc111055d0769201f30ddfcdfadf1be2
moi <anton.perdoncin@protonmail.com>
2020-03-04 00:44:05 +0100

    EDIT  page présentation

--- src/pages/Presentation.vue
+++ src/pages/Presentation.vue
@@ -11,11 +11,14 @@
 						et les retraites : tous manifestent leur désir de changement et bousculent
 						la vie politique, à Nantes comme dans le reste du pays.
 					</p>
+					<p class="article__summary">
+					Voter pour une liste anticapitaliste et révolutionnaire c'est <span class="bold">faire entendre la voix de celles et ceux qui luttent</span>. C'est <span class="bold">faire entendre les préoccupations et les intérêts de celles et ceux que l'on n'écoute jamais</span>: les pauvres, les oppriméEs, les exploitéEs.
+					</p>
 				</div>
 			</div>
 			<div class="fit-article article__content">
 				<a class="anchor" id="municipales-nantes"></a>
-				<h3>Face au gouvernement des riches, organisons nos luttes</h3>
+				<h2>Face au gouvernement des riches, organisons nos luttes</h2>
 				<p>
 					Ces élections municipales se déroulent dans un climat de tempête sociale et politique. Emmanuel Macron, dans la continuité des gouvernements précédents, de gauche comme de droite, poursuit ses attaques contre le monde du travail et les pauvres. Mais il a du fil à retordre.
 				</p> 
@@ -31,58 +34,61 @@
 
 				<h3>Nos vies sacrifiées à leurs profits, c'est à Nantes aussi !</h3>
 				<p>
-					Nantes est une ville riche, mais elle est aussi inégalitaire. A Nantes comme ailleurs les licenciements ne sont pas rares et l'on manque d'argent et de personnel dans tous les services publics. Comme au CHU, qui est en saturation par manque de moyens, à quoi s'ajoute le projet inutile de son déplacement. 
+					<span class="bold">Nantes est une ville riche, mais elle est aussi inégalitaire</span>. A Nantes comme ailleurs les licenciements ne sont pas rares et l'<span class="bold">on manque d'argent et de personnel dans tous les services publics</span>. Comme au CHU, qui est en saturation par manque de moyens, à quoi s'ajoute le projet inutile de son déplacement. 
 				</p> 
 				<p>
-					Ce ne sont pourtant pas les moyens financiers qui manquent. Les grandes entreprises françaises du CAC40 affichent des gains époustouflants et en hausse : ils ont distribué 60 milliards à leurs actionnaires en 2019, alors que, avec cette somme, on pourrait créer plus d'un million et demi d'emplois ! De quoi résoudre une grande partie des problèmes des hôpitaux et des écoles !
+					Ce ne sont pourtant pas les moyens financiers qui manquent. <span class="bold">Les grandes entreprises françaises du CAC40 affichent des gains époustouflants et en hausse</span> : ils ont distribué 60 milliards à leurs actionnaires en 2019, alors que, avec cette somme, on pourrait créer plus d'un million et demi d'emplois ! De quoi résoudre une grande partie des problèmes des hôpitaux et des écoles !
 				</p> 
 				<p>
-					Macron, lui, a préféré arroser les patrons et les riches de cadeaux fiscaux, dont la suppression de l'impôt sur la fortune. Et les grandes villes comme Nantes sont d'énormes gâteaux financiers, dont les budgets arrosent les sociétés de BTP, d'autoroutes, en laissant des miettes pour le « social ».
+					Macron, lui, a préféré arroser les patrons et les riches de cadeaux fiscaux, dont la suppression de l'impôt sur la fortune. Et <span class="bold">les grandes villes comme Nantes sont d'énormes gâteaux financiers</span>, dont les budgets arrosent les sociétés de BTP, d'autoroutes, en laissant des miettes pour le « social ».
 				</p>
 
 				<a class="anchor" id="programme-lutter"></a>
 				<h3>Un programme pour lutter</h3>
 				<p>
-					Certains appellent à voter tout sauf Macron et ses relais locaux. D'autres à voter Macron pour éviter Le Pen. Comme si l'on devait choisir entre un banquier avec des idées racistes et une raciste amie des banquiers ! D'autres encore prétendent pouvoir résoudre localement les tares de cette société. Pourtant, qu'on le veuille ou non, les problèmes locaux, nationaux et internationaux sont mêlés.
+					Certains appellent à voter tout sauf Macron et ses relais locaux. D'autres à voter Macron pour éviter Le Pen. Comme si l'on devait choisir entre un banquier avec des idées racistes et une raciste amie des banquiers ! D'autres encore prétendent pouvoir résoudre localement les tares de cette société. Pourtant, qu'on le veuille ou non, <span class="bold">les problèmes locaux, nationaux et internationaux sont mêlés.</span>
 				</p> 
 				<p>
-					Voter, surtout aux élections municipales, ne réglera pas les problème du chômage, des salaires en berne, du manque de logements, de la misère des hôpitaux et des écoles, de l'oppression des femmes, du racisme... Loin des promesses électorales, la véritable opposition à Macron ce sont les travailleuses et les travailleurs en lutte, et démocratiquement organisés pour la défense de revendications urgentes et vitales. Mais nous pouvons profiter de ces élections pour dire bien fort notre rejet de leur système !
+					Voter, surtout aux élections municipales, ne réglera pas les problème du chômage, des salaires en berne, du manque de logements, de la misère des hôpitaux et des écoles, de l'oppression des femmes, du racisme... <span class="bold">Loin des promesses électorales, la véritable opposition à Macron ce sont les travailleuses et les travailleurs en lutte, et démocratiquement organisés pour la défense de revendications urgentes et vitales.</span> 
+				</p> 
+				<p>
+					Mais nous pouvons profiter de ces élections pour <span class="bold">dire bien fort notre rejet de leur système</span>  !
 				</p> 
 
 				<h4>Interdiction des licenciements et des suppressions de postes</h4>
 				<p> 
-					Michelin, Auchan, Carrefour, Renault... Les grands groupes multiplient les annonces de suppressions d'emplois, à Nantes comme ailleurs. À la destruction des emplois, à l'explosion de la précarité, il faut opposer une politique générale de création d'emplois socialement utiles, notamment dans les services publics. Travaillons tous, moins et mieux.
+					Michelin, Auchan, Carrefour, Renault... Les grands groupes multiplient les annonces de suppressions d'emplois, à Nantes comme ailleurs. À la destruction des emplois, à l'explosion de la précarité, il faut opposer une politique générale de création d'emplois socialement utiles, notamment dans les services publics. <span class="bold">Travaillons tous, moins et mieux.</span>
 				</p> 
 
 				<h4>Plafonnement des loyers et réquisition des logements vides</h4>
 				<p> 
-					Les loyers à Nantes sont devenus hors de portée, et pas seulement dans le centre-ville. L'accès à la propriété est presque impossible. Que va-t-il se passer ? Nous ne pourrons plus payer les loyers, et nous devrons refuser de les payer.
+					Les loyers à Nantes sont devenus hors de portée, et pas seulement dans le centre-ville. L'accès à la propriété est presque impossible. Que va-t-il se passer ? <span class="bold">Nous ne pourrons plus payer les loyers, et nous devrons refuser de les payer.</span>
 				</p> 
 				<p> 
-					Faire cesser la spéculation immobilière et l'exode des plus modestes hors de la ville, nécessitera l'organisation des locataires pour imposer des loyers décents par une lutte massive. Il faut utiliser les terrains vacants pour construire des logements sociaux et les services qui vont avec, privilégier la réhabilitation de ce qui peut l'être et arrêter les démolitions.
+					Faire cesser la spéculation immobilière et l'exode des plus modestes hors de la ville, nécessitera l'organisation des locataires pour imposer des loyers décents par une lutte massive. Il faut <span class="bold">utiliser les terrains vacants pour construire des logements sociaux et les services qui vont avec</span>, privilégier la réhabilitation de ce qui peut l'être et arrêter les démolitions.
 				</p> 
 
 				<h4>Pas un salaire, pas une pension, pas un minimum social en dessous de 1 800 euros nets. Augmentation des salaires de 300 euros par mois pour toutes et tous </h4>
 				<p> 
-					Tout augmente sauf les salaires. À Nantes le chômage est élevé. Comme partout de plus en plus de gens sont obligés de combiner plusieurs emplois à temps partiel ou d'empiler les contrats en intérim. Cette explosion du travail précaire touche tous les secteurs, de l'aéronautique à la logistique en passant par les services ou le bâtiment. 
+					<span class="bold">Tout augmente sauf les salaires. À Nantes le chômage est élevé.</span> Comme partout de plus en plus de gens sont obligés de combiner plusieurs emplois à temps partiel ou d'empiler les contrats en intérim. Cette explosion du travail précaire touche tous les secteurs, de l'aéronautique à la logistique en passant par les services ou le bâtiment. 
 				</p>
 
 				<h4>Prendre sur les profits pour défendre l'environnement</h4>
 				<p> 
-					Dans cette campagne, c'est à qui se repeindra le plus en vert. Mais le capitalisme ne sera jamais vert : l'écologie sert uniquement de prétexte pour justifier, d'un côté de nouvelles subventions aux entreprises, et de l'autre de nouvelles taxes et des licenciements pour les classes populaires.
+					Dans cette campagne, c'est à qui se repeindra le plus en vert. Mais <span class="bold"><span class="bold">le capitalisme ne sera jamais vert</span> : l'écologie sert uniquement de prétexte pour justifier, d'un côté de nouvelles subventions aux entreprises, et de l'autre de nouvelles taxes et des licenciements pour les classes populaires.
 				</p> 
 				<p> 
-					Seuls les travailleurs peuvent assurer la transparence qui garantit la sécurité des sites industriels classés à risques (comme celui de Saint-Herblain), pour eux-mêmes et pour les habitants. L'incendie de Lubrizol à Rouen est un avertissement pour touTEs : notre santé est le cadet de leurs soucis. 
+					<span class="bold"><span class="bold">Seuls les travailleurs peuvent assurer la transparence qui garantit la sécurité des sites industriels classés à risques</span> (comme celui de Saint-Herblain), pour eux-mêmes et pour les habitants. L'incendie de Lubrizol à Rouen est un avertissement pour touTEs : notre santé est le cadet de leurs soucis. 
 				</p>
 
 				<h4>Contre les violences policières</h4>
 				<p> 
-					À Nantes on peut mourir lors de la fête de la musique après une charge de police. On peut y perdre un œil, être frappé brutalement, arrêté pour avoir manifesté. Que l'on soit un ancien ou un jeune, les violences policières sont la seule réponse du gouvernement à l'urgence sociale. Alors que la plupart des candidatEs à la mairie de Nantes se sont lancéEs dans une escalade sécuritaire, il faut le dire : la répression ne résoudra pas la misère sociale.
+					À Nantes on peut mourir lors de la fête de la musique après une charge de police. On peut y perdre un œil, être frappé brutalement, arrêté pour avoir manifesté. <span class="bold">Que l'on soit un ancien ou un jeune, les violences policières sont la seule réponse du gouvernement à l'urgence sociale</span>. Alors que la plupart des candidatEs à la mairie de Nantes se sont lancéEs dans une escalade sécuritaire, il faut le dire : <span class="bold">la répression ne résoudra pas la misère sociale</span>.
 				</p> 
 
 				<h4>Contre les discriminations et les violences sexistes</h4>
 				<p> 
-					En 2019, au moins 150 femmes ont été assassinées en France. À Nantes, des étudiantes, des travailleuses, des Gilets Jaunes, des retraitées, ont témoigné massivement leur indignation le 3 octobre puis le 25 novembre dernier. Elles ont dit non aux meurtres de femmes parce qu'elles sont des femmes et aux violences sexuelles et sexistes, qui sont les manifestations extrêmes de l'oppression des femmes dans tous les domaines de la société : emploi, vie de famille, représentation démocratique... 
+					En 2019, au moins 150 femmes ont été assassinées en France. À Nantes, des étudiantes, des travailleuses, des Gilets Jaunes, des retraitées, ont témoigné massivement leur indignation le 3 octobre puis le 25 novembre dernier. Elles ont dit <span class="bold">non aux meurtres de femmes parce qu'elles sont des femmes et aux violences sexuelles et sexistes, qui sont les manifestations extrêmes de l'oppression des femmes dans tous les domaines de la société</span> : emploi, vie de famille, représentation démocratique... 
 				</p> 
 				<p>
 					Il faut défendre non seulement l'égalité des droits et des salaires, mais aussi la dignité que nie quotidiennement le système patriarcal.
@@ -90,21 +96,21 @@
 
 				<h4>De l'air, ouvrez les frontières : liberté de circulation et d'installation pour toutes et tous</h4>
 				<p> 
-					Le gouvernement – et ses relais locaux – orchestre une campagne raciste à l'encontre des migrants et des musulmans, directement inspirée de l'extrême-droite. Cette politique détourne l'attention des questions sociales, mais vise aussi à faire monter la haine, à enrôler les pauvres contre les très pauvres, les déclassés contre les plus vulnérables. Pourtant, ce ne sont ni les Roms, ni les migrants, ni les musulmans qui suppriment nos emplois et saccagent nos services publics. 
+					Le gouvernement – et ses relais locaux – orchestre une campagne raciste à l'encontre des migrants et des musulmans, directement inspirée de l'extrême-droite. Cette politique détourne l'attention des questions sociales, mais vise aussi à faire monter la haine, à enrôler les pauvres contre les très pauvres, les déclassés contre les plus vulnérables. Pourtant, <span class="bold">ce ne sont ni les Roms, ni les migrants, ni les musulmans qui suppriment nos emplois et saccagent nos services publics</span>. 
 				</p> 
 				<p>
-					La préfecture et la mairie se renvoient la balle pour justifier leurs politiques d'expulsion. Mais à Nantes comme ailleurs, les moyens existent d'accueillir dignement touTEs les migrantEs.
+					La préfecture et la mairie se renvoient la balle pour justifier leurs politiques d'expulsion. Mais <span class="bold">à Nantes comme ailleurs, les moyens existent d'accueillir dignement touTEs les migrantEs</span>.
 				</p> 
 
 				<h3>Votez et faites voter pour la liste Anticapitalistes et Révolutionnaires ! Nantes aux travailleuses et aux travailleurs</h3>
 				<p>
-					Dans ces élections municipales, la liste présentée par le NPA veut porter toutes les exigences issues des luttes, locales et nationales : celles des cheminotEs et de l'ensemble du monde du travail contre la réforme des retraites, celles des personnels des universités contre la précarité, des enseignants et des élèves des lycées contre le bac Blanquer et le tri social à l'école, celles des hospitaliers pour les salaires et des embauches.
+					Dans ces élections municipales, la liste présentée par le NPA veut <span class="bold">porter toutes les exigences issues des luttes, locales et nationales</span> : celles des cheminotEs et de l'ensemble du monde du travail contre la réforme des retraites, celles des personnels des universités contre la précarité, des enseignants et des élèves des lycées contre le bac Blanquer et le tri social à l'école, celles des hospitaliers pour les salaires et des embauches.
 				</p> 
 				<p>
-					Notre liste est à l'image de la majorité des habitantes et des habitants de cette ville. Elle compte des salariés, des travailleurs en galère et en situation précaire, des étudiantEs, des retraitéEs. Elle compte des hommes et des femmes de tous âges qui ont participé à l'organisation des grèves et des mobilisations dans leurs secteurs, et qui ont à cœur les intérêts de leur classe sociale.
+					<span class="bold">Notre liste est à l'image de la majorité des habitantes et des habitants de cette ville</span>. Elle compte des salariés, des travailleurs en galère et en situation précaire, des étudiantEs, des retraitéEs. Elle compte des hommes et des femmes de tous âges qui ont participé à l'organisation des grèves et des mobilisations dans leurs secteurs, et qui ont à cœur les intérêts de leur classe sociale.
 				</p> 
 				<p>
-					Nos élus au conseil municipal s'engagent à y défendre les intérêts des travailleuses et des travailleurs de la ville et de ses quartiers populaires. ils se serviront de tous les moyens à leur disposition pour être les relais des luttes et aider à leur organisation et à leur victoire.
+					Nos élus au conseil municipal s'engagent à y <span class="bold">défendre les intérêts des travailleuses et des travailleurs de la ville et de ses quartiers populaires</span>. ils se serviront de tous les moyens à leur disposition pour être les relais des luttes et aider à leur organisation et à leur victoire.
 				</p> 
 
 				<a class="anchor" id="qui-sommes-nous"></a>
